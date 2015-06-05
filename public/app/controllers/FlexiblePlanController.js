a713279756e40195a53d18825e4f91c6b2ce64bd
Alexandra Popescu <lixapopescu@gmail.com>
2015-06-06 01:46:53 +0200

    add flexible planning. just recipe list for now

--- public/app/controllers/FlexiblePlanController.js
+++ public/app/controllers/FlexiblePlanController.js
@@ -1,7 +1,7 @@
-var fillCustomAttributesPlan = function($scope, dailyPlans) {
+var fillCustomAttributesPlan = function(dailyPlans, today) {
     _.each(dailyPlans, function(dailyPlan) {
         dailyPlan.date = new Date(dailyPlan.date);
-        dailyPlan.dayDistance = Math.round((dailyPlan.date - $scope.today) / millisecondsInDay);
+        dailyPlan.dayDistance = Math.round((dailyPlan.date - today) / millisecondsInDay);
         dailyPlan.dayLabel = getWeekDay(dailyPlan.date.getDay(), 's');
         if (!!weekDistanceLabels[dailyPlan.dayDistance])
             dailyPlan.dayDistanceLabel = weekDistanceLabels[dailyPlan.dayDistance];
@@ -10,6 +10,12 @@ var fillCustomAttributesPlan = function($scope, dailyPlans) {
             dailyPlan.dayDistanceLabel = dist;
         }
     });
+}
+
+var attachAbbrev = function(dailyPlans) {
+    _.each(_.sortBy(dailyPlans, 'date'), function(dailyPlan, index) {
+        dailyPlan.abbrev = String.fromCharCode(65 + index);
+    })
 }
 
 var FlexiblePlanController = function($http, $stateParams, $scope) {
@@ -21,7 +27,8 @@ var FlexiblePlanController = function($http, $stateParams, $scope) {
     $http.get('api/plan' + apiPath)
         .success(function(data) {
             console.log(data);
-            fillCustomAttributesPlan($scope, data);
+            fillCustomAttributesPlan(data, $scope.today);
+            attachAbbrev(data);
             $scope.dailyPlans = data;
         });
 }
